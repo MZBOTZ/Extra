ca1c0d455e5dccef8afb4f7b10eef60340895a70
Inblizbot <108299646+Inblizbot@users.noreply.github.com>
2022-08-11 20:59:25 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -75,7 +75,7 @@ async def start(client, message):
             InlineKeyboardButton('🕹️ CHANNEL 🕹️', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
             InlineKeyboardButton(' HELP ', callback_data='help'),
-            InlineKeyboardButton(' SHARE ', callback_data='about')
+            InlineKeyboardButton(' ABOUT ', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
